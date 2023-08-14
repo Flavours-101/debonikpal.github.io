042a42b4ba29cdacea8fd208d4cd2aa6e0d6dc6a
Debonik Pal <73780823+Debonik@users.noreply.github.com>
2023-08-14 16:51:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Debonik Pal', // Required
+  description: 'Marketer:Web Developer:Product Enthusiast',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://ryanfitzgerald.ca',
+    github: 'https://github.com/ryanfitzgerald',
+    twitter: 'https://twitter.com/ryanafitzgerald',
+    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
